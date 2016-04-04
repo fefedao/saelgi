93fdcf3e6368227a677a56f10e2ccaf3334376af
fefedo <fefedo@battycode.com.br>
2016-04-03 22:52:04 -0300

    logon page

--- saelgi/src/test/java/br/com/battycode/test/SaelgiApplicationTests.java
+++ saelgi/src/test/java/br/com/battycode/test/SaelgiApplicationTests.java
@@ -2,15 +2,14 @@ package br.com.battycode.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import br.com.battycode.config.SaelgiApplication;
-
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import br.com.battycode.endpoint.SaelgiEndpoint;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SaelgiApplication.class)
+@SpringApplicationConfiguration(classes = SaelgiEndpoint.class)
 @WebAppConfiguration
 public class SaelgiApplicationTests {
 
