32dce0c911903cb080e5e3beddbfd98bfe1d1ce6
fefedao <luiz.hartke@gmail.com>
2016-09-20 13:55:03 -0300

    inicio implementacao blob hsqldb teste

--- saelgi/src/main/java/br/com/battycode/service/impl/RepresentanteServiceImpl.java
+++ saelgi/src/main/java/br/com/battycode/service/impl/RepresentanteServiceImpl.java
@@ -4,9 +4,11 @@ import br.com.battycode.dto.Representante;
 import br.com.battycode.jpa.repository.RepresentanteRepository;
 import br.com.battycode.service.RepresentanteService;
 import com.google.common.collect.Lists;
+import com.sendgrid.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,6 +49,28 @@ public class RepresentanteServiceImpl implements RepresentanteService {
     @Override
     public Representante obterRepresentante(Integer codigo) {
         return representanteRepository.findOne(codigo);
+    }
+
+    public void enviarEmailRepresentanteLicitacao(){
+        Email from = new Email("test@example.com");
+        String subject = "Hello World from the SendGrid Java Library!";
+        Email to = new Email("test@example.com");
+        Content content = new Content("text/plain", "Hello, Email!");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.method = Method.POST;
+            request.endpoint = "mail/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            System.out.println(response.statusCode);
+            System.out.println(response.body);
+            System.out.println(response.headers);
+        } catch (IOException ex) {
+            throw ex;
+        }
     }
 
 }
